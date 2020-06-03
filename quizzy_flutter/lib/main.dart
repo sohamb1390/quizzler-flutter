09bb5aa911434509067e37fe5f8f72a1740924bb
Soham Bhattacharjee <sohamb.1390@gmail.com>
2020-06-03 12:41:08 +0530

    Code seperation

--- quizzy_flutter/lib/main.dart
+++ quizzy_flutter/lib/main.dart
@@ -25,26 +25,77 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  Widget trueFlatButton() {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: FlatButton(
+          color: Colors.green,
+          onPressed: () {
+            //The user picked true.
+          },
+          child: Text(
+            'True',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget flaseFlatButton() {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: FlatButton(
+          color: Colors.red,
+          onPressed: () {
+            //The user picked true.
+          },
+          child: Text(
+            'False',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget questionText() {
+    return Expanded(
+      flex: 5,
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Center(
+          child: Text(
+            'This is where the question text will go',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Center(
-                child: Text(
-                  'This is where the question text will go',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25.0,
-                  ),
-                ),
-              ),
-            ),
-          )
+          questionText(),
+          trueFlatButton(),
+          flaseFlatButton(),
         ],
       ),
     );
